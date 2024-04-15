a836c23076b6080c66fdcde6f7afe4e321e8e57b
harry <harrispap03@gmail.com>
2024-04-15 15:23:41 +0300

    test: make tests pass

--- src/cats/cats.controller.spec.ts
+++ src/cats/cats.controller.spec.ts
@@ -1,34 +1,45 @@
-import { Test } from '@nestjs/testing';
-import { CatsController } from './cats.controller';
-import { CatsService } from './cats.service';
-import { Cat } from './interfaces/cat.interface';
+import { Test } from "@nestjs/testing";
+import { CatsController } from "./cats.controller";
+import { CatsService } from "./cats.service";
+import { Cat } from "./entities/cat.entity"; 
 
-describe('CatsController', () => {
+describe("CatsController", () => {
   let catsController: CatsController;
   let catsService: CatsService;
 
   beforeEach(async () => {
     const moduleRef = await Test.createTestingModule({
       controllers: [CatsController],
-      providers: [CatsService],
+      providers: [{ provide: CatsService, useValue: new MockCatsService() }],
     }).compile();
 
     catsService = moduleRef.get<CatsService>(CatsService);
     catsController = moduleRef.get<CatsController>(CatsController);
   });
 
-  describe('findAll', () => {
-    it('should return an array of cats', async () => {
+  describe("findAll", () => {
+    it("should return an array of cats", async () => {
       const result: Cat[] = [
         {
+          id: 1,
           age: 2,
-          breed: 'Bombay',
-          name: 'Pixel',
+          breed: "Bombay",
+          name: "Pixel",
+          createdAt: new Date(),
+          updatedAt: new Date(), 
         },
       ];
-      jest.spyOn(catsService, 'findAll').mockImplementation(() => result);
 
+      jest
+        .spyOn(catsService, "findAll")
+        .mockImplementation(() => Promise.resolve(result));
       expect(await catsController.findAll()).toBe(result);
     });
   });
 });
+
+class MockCatsService {
+  async findAll(): Promise<Cat[]> {
+    return [];
+  }
+}
--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,12 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
 
 describe('UserService', () => {
   let service: UserService;
+  let mockRepository;
 
   beforeEach(async () => {
+    mockRepository = {
+      find: jest.fn(),
+      findOneBy: jest.fn(),
+      save: jest.fn(),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [
+        UserService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: mockRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<UserService>(UserService);
